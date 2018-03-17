76902381e180852343b56eaf473b9b350a59decd
tober <zhyiuwai@outlook.com>
2018-03-17 13:49:46 +0800

    添加saf参数

--- standard.py
+++ standard.py
@@ -12,7 +12,7 @@ industry = pd.read_pickle('C:/Users/wuwangchuxin/Desktop/yinhua_\
 return_data = pd.read_pickle('G:short_period_mf/dailyreturn.pick\
     le').rename(columns={'symbol':'code'})
 
-def Neutral_process(alpha_data):
+def Neutral_process(alpha_data, saf):
     num_mint = alpha_data.shape[1]
     num_inds = industry.shape[1]
     data = pd.merge(alpha_data, industry, on=['code']).dropna()
@@ -25,7 +25,7 @@ def Neutral_process(alpha_data):
     output.close()
     retutn 0
 
-def IC_computing(alpha_data):
+def IC_computing(alpha_data, saf):
     data = pd.merge(alpha_data,return_data,on = ['code'])
     dailyReturn = data.daily_return
     factors = Netual_process(alpha_data)
@@ -35,12 +35,14 @@ def IC_computing(alpha_data):
     output.close()    
     return 0
 
+
+################
 standard_alpha = os.listdir(r'G:/short_period_mf/alpha_min_stand')
 for saf in standard_alpha:
-    path = r'G:/short_period_mf/alpha_min_stand/%s'%saf
-    IC_computing(path,industry)
+    alpha_d = pd.read_pickle(r'G:/short_period_mf/alpha_min_stand/%s'%saf)
+    IC_computing(alpha_d, saf)
 
 
 for saf in standard_alpha:
     alpha_d = pd.read_pickle(r'G:/short_period_mf/alpha_min_stand/%s'%saf)
-    Neutral_process(alpha_d,industry3)
+    Neutral_process(alpha_d,saf)
