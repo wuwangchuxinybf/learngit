bca56247c0d868fdfa8796b9dfb6069e6e630156
tober <zhyiuwai@outlook.com>
2018-03-17 16:45:32 +0800

    修正中心化函数错误

--- standard.py
+++ standard.py
@@ -12,23 +12,26 @@ industry = pd.read_pickle('C:/Users/wuwangchuxin/Desktop/yinhua_\
 return_data = pd.read_pickle('G:short_period_mf/dailyreturn.pick\
     le').rename(columns={'symbol':'code'})
 
+def resid(x, y):
+    return sm.OLS(x, y).fit().resid
+    
 def Neutral_process(alpha_data, saf):
     num_mint = alpha_data.shape[1]
     num_inds = industry.shape[1]
     data = pd.merge(alpha_data, industry, on=['code']).dropna()
-    X = data.iloc[:, [1:num_mint+1]]
-    y = data.iloc[:, [num_mint+1:num_mint+num_inds+1]]
-    X.apply(lambda x: sm.OLS(X, y).fit.resid, inplace=True)
+    X = data.iloc[:, 1:num_mint]
+    y = data.iloc[:, num_mint:num_mint+num_inds]
+    X = X.apply(lambda x:resid(x, y))
     X['code'] = alpha_data['code']
     output = open(r'G:/short_period_mf/netual_process/netual_%s'%saf[-16:],'wb')
     pickle.dump(X,output)
     output.close()
-    retutn 0
+    return 0
 
 def IC_computing(alpha_data, saf):
     data = pd.merge(alpha_data,return_data,on = ['code'])
     dailyReturn = data.daily_return
-    factors = Netual_process(alpha_data)
+    factors = Neutral_process(alpha_data)
     IC = dailyReturn.corr(factors, method='spearman')
     output = open(r'G:/short_period_mf/ic_value/ic_%s'%saf[-16:],'wb')
     pickle.dump(IC,output)
@@ -41,7 +44,6 @@ standard_alpha = os.listdir(r'G:/short_period_mf/alpha_min_stand')
 for saf in standard_alpha:
     alpha_d = pd.read_pickle(r'G:/short_period_mf/alpha_min_stand/%s'%saf)
     IC_computing(alpha_d, saf)
-
 
 for saf in standard_alpha:
     alpha_d = pd.read_pickle(r'G:/short_period_mf/alpha_min_stand/%s'%saf)
